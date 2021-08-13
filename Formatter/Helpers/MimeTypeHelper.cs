48a27e3a12a7ab1185d1a5a15c6e6e105e8c9313
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2021-08-13 17:18:00 +0200

    Ajoutez des fichiers projet.

---
+++ Formatter/Helpers/MimeTypeHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Formatter.Helpers
+{
+    public static class MimeTypeHelper
+    {
+        /// <summary>
+        /// Get a pointer value on the passed bytes array
+        /// </summary>
+        /// <param name="pBC"></param>
+        /// <param name="pwzUrl"></param>
+        /// <param name="pBuffer"></param>
+        /// <param name="cbSize"></param>
+        /// <param name="pwzMimeProposed"></param>
+        /// <param name="dwMimeFlags"></param>
+        /// <param name="ppwzMimeOut"></param>
+        /// <param name="dwReserved"></param>
+        /// <returns><see cref="IntPtr"/> pointer</returns>
+        [DllImport("urlmon.dll", CharSet = CharSet.Unicode, ExactSpelling = true, SetLastError = false)]
+        public static extern int FindMimeFromData(IntPtr pBC,
+        [MarshalAs(UnmanagedType.LPWStr)] string pwzUrl,
+        [MarshalAs(UnmanagedType.LPArray, ArraySubType=UnmanagedType.I1, SizeParamIndex=3)]
+        byte[] pBuffer,
+        int cbSize,
+        [MarshalAs(UnmanagedType.LPWStr)] string pwzMimeProposed,
+        int dwMimeFlags,
+        out IntPtr ppwzMimeOut,
+        int dwReserved);
+    }
+}
---
+++ Formatter/src/Formatter.cs
@@ -0,0 +1,55 @@
+﻿using Formatter.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Formatter.src
+{
+    public static class Formatter
+    {
+        /// <summary>
+        ///  Get file's extension its from bytes array.
+        /// </summary>
+        /// <param name="image"><see cref="byte"/>The array containing image bytes converted</param>
+        /// <returns><see cref="string"/> extension</returns>
+        public static string Extension(byte[] image)
+        {
+            try
+            {
+                _ =MimeTypeHelper.FindMimeFromData(IntPtr.Zero, null, image, image.Length, null, 0, out var extension, 0);
+
+                var mime = Marshal.PtrToStringUni(extension);
+                Marshal.FreeCoTaskMem(extension);
+
+                return mime ?? null;
+
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Allows to format file to base64 format for displaying purpose
+        /// </summary>
+        /// <param name="file">The file byte array</param>
+        /// <returns><see cref="string"/> fromatted file</returns>
+        public static string Format(byte[] file)
+        {
+            //get the file extension
+            var fileExtension = Extension(file);
+            var encodeToBase64 = Convert.ToBase64String(file);//convert bytes array to base64 format
+            var formattedFile = $"data:{fileExtension};base64,{encodeToBase64}";
+
+            return formattedFile ?? null;
+        }
+
+        public static string Format(string path)
+        {
+
+        }
+    }
+}
