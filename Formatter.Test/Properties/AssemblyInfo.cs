ae1aa197cd20d0f9231ed14ef38c85255c327d59
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2021-08-13 19:05:23 +0200

    🚧Test all methods

---
+++ Formatter.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Formatter.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Formatter.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b9c4108f-1577-4d67-8c7a-70b763873aec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Formatter.Test/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Formatter.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestImageByteArray()
+        {
+            var imgArray = File.ReadAllBytes(@"C:\Users\CTRL TECH\Pictures\Ambulance.jpg");
+            var result = src.Formatter.GetExtension(imgArray);
+
+            Assert.IsInstanceOfType(result, typeof(string));
+        }
+
+        [TestMethod]
+        public void TestImageFormatting()
+        {
+            var imgArray = File.ReadAllBytes(@"C:\Users\CTRL TECH\Pictures\Ambulance.jpg");
+            var result = src.Formatter.Format(imgArray);
+
+            Assert.IsInstanceOfType(result, typeof(string));
+        }
+
+        [TestMethod]
+        public void TestImageFormattingFromPath()
+        {
+            var result = src.Formatter.Format(@"C:\Users\CTRL TECH\Pictures\Ambulance.jpg");
+
+            Assert.IsInstanceOfType(result, typeof(string));
+        }
+
+        [TestMethod]
+        public void TestOtherFiles()
+        {
+            var fileArrayBytes = src.Formatter.GetExtension(@"C:\Users\CTRL TECH\Documents\Fonctionalitees.docx");
+            Assert.IsInstanceOfType(fileArrayBytes, typeof(string));
+        }
+
+        [TestMethod]
+        public void TestOtheFileByArrayGetExtension()
+        {
+            var result = src.Formatter.GetExtension(@"C:\Users\CTRL TECH\Documents\Fonctionalitees.docx");
+
+            Assert.IsInstanceOfType(result, typeof(string));
+        }
+
+        [TestMethod]
+        public void TestOtherFileFormatting()
+        {
+            var result = src.Formatter.Format(@"C:\Users\CTRL TECH\Documents\Fonctionalitees.docx");
+
+            Assert.IsInstanceOfType(result, typeof(string));
+        }
+    }
+}
